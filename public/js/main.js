9ef9b0ae6055381cf459d0b6da347db002959bc3
Qlexe <105638501+Qlexe@users.noreply.github.com>
2023-12-19 10:54:44 +0000

    new

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,42 @@
-setInterval(() => {location.reload()}, 2000)
-
-let url = "https://webchat.oleksandrkap.repl.co/messages/";
-let data = [];
-  fetch(url)
-  .then((response) => {response.json()});
-  .then((data) => {
-  .then((result) => console.log(result));
-
-
+// setInterval(() => {location.reload()}, 2000)
 
 let input = document.querySelector("#message_input");
-  let add = document.querySelector("#message_add")
+let add = document.querySelector("#message_add")
 if (!localStorage.getItem("user")) {
-  input.placeholder = "Як вас звати? Потім натисніть кнопку 'Ввести'.";
-  input.addEventListener("keydown", function(event) {
-    if (event.code === "Enter") {
-        if (input.value.length > 2) {
-          localStorage.setItem("user", JSON.stringify(input.value));
-        } else {
-          return alert("Введіть ім'я більше 3 символів");
+    input.placeholder = "Як вас звати? Потім натисніть кнопку 'Ввести'.";
+    input.addEventListener("keydown", function (event) {
+        if (event.code === "Enter") {
+            if (input.value.length > 2) {
+                localStorage.setItem("user", JSON.stringify(input.value));
+            } else {
+                return alert("Введіть ім'я більше 3 символів");
+            }
         }
-    }
-  });
-  input.removeEventListener("keydown")
+    });
+    // input.removeEventListener("keydown")
 } else {
-  input.placeholder = JSON.parse(localStorage.getItem("user", localStorage.getItem("user")));
+    input.placeholder = JSON.parse(localStorage.getItem("user", localStorage.getItem("user")));
 }
 
 // WRITING MESSAGES
-add.addEventListener("click", function() {
-  fetch(db)
+add.addEventListener("click", function () {
+    const url = "https://webchat.oleksandrkap.repl.co/messages/";
+    // let data = [];
+    let userData = {
+        user: JSON.parse(localStorage.getItem("user")),
+        message: input.value,
+        time: new Date().toLocaleString()
+    };
+
+    const requestOptions = {
+        method: "POST", // Вказуємо метод PUT
+        headers: {
+            "Content-Type": "application/json" // Вказуємо тип вмісту JSON
+        },
+        body: JSON.stringify(userData) // Перетворюємо дані в рядок JSON
+    };
+
+    fetch(url, requestOptions)
+        .then(data => console.log(data)); // Робимо щось з даними
+        // .catch(error => console.error(error)); // Обробляємо помилки
+})
